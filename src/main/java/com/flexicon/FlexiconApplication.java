25bfa852913c6dcb0bf8f8d03798d319796c21a9
Isaac Fisher <isaac.fisher@nyu.edu>
2024-07-29 11:00:48 -0400

    initial commit, spring boot initializr

---
+++ src/main/java/com/flexicon/FlexiconApplication.java
@@ -0,0 +1,13 @@
+package com.flexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexiconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexicon/FlexiconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexiconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
